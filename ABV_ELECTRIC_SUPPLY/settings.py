d97c5bce4e72faa85d64a05c19731bc91326737f
OctavioRodriguez83 <octavio.trinidad.rodriguez@abvelectricsupply.com>
2025-11-10 13:22:08 -0600

    Update ALLOWED_HOSTS configuration in settings.py

--- ABV_ELECTRIC_SUPPLY/settings.py
+++ ABV_ELECTRIC_SUPPLY/settings.py
@@ -11,8 +11,7 @@ LOGIN_URL = '/login/'
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1:8000,localhost').split(',')
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1:8000,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
